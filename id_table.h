e7dac4529471862280f297f7b45a627e8adce825
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-07-29 01:11:21 -0700

    Initialize iclass-wb

---
+++ id_table.h
@@ -0,0 +1,54 @@
+#ifndef RUBY_ID_TABLE_H
+#define RUBY_ID_TABLE_H 1
+#include "ruby/internal/config.h"
+#include <stddef.h>
+#include "ruby/ruby.h"
+
+struct rb_id_table;
+
+/* compatible with ST_* */
+enum rb_id_table_iterator_result {
+    ID_TABLE_CONTINUE = ST_CONTINUE,
+    ID_TABLE_STOP     = ST_STOP,
+    ID_TABLE_DELETE   = ST_DELETE,
+    ID_TABLE_REPLACE  = ST_REPLACE,
+    ID_TABLE_ITERATOR_RESULT_END
+};
+
+struct rb_id_table {
+    int capa;
+    int num;
+    struct rb_id_item *items;
+    int used;
+};
+
+/* id_table imemo */
+struct rb_managed_id_table {
+    VALUE flags;
+    struct rb_id_table table;
+};
+
+struct rb_id_table *rb_id_table_create(size_t size);
+struct rb_id_table *rb_id_table_init(struct rb_id_table *tbl, int capa);
+void rb_id_table_free(struct rb_id_table *tbl);
+void rb_id_table_clear(struct rb_id_table *tbl);
+
+size_t rb_id_table_size(const struct rb_id_table *tbl);
+size_t rb_id_table_memsize(const struct rb_id_table *tbl);
+
+struct rb_managed_id_table *rb_managed_id_table_new(int memo_type, size_t capa);
+void rb_managed_id_table_free(struct rb_managed_id_table *tbl);
+size_t rb_managed_id_table_memsize(const struct rb_managed_id_table *tbl);
+
+int rb_id_table_insert(struct rb_id_table *tbl, ID id, VALUE val);
+int rb_id_table_lookup(struct rb_id_table *tbl, ID id, VALUE *valp);
+int rb_id_table_delete(struct rb_id_table *tbl, ID id);
+
+typedef enum rb_id_table_iterator_result rb_id_table_update_callback_func_t(ID *id, VALUE *val, void *data, int existing);
+typedef enum rb_id_table_iterator_result rb_id_table_foreach_func_t(ID id, VALUE val, void *data);
+typedef enum rb_id_table_iterator_result rb_id_table_foreach_values_func_t(VALUE val, void *data);
+void rb_id_table_foreach(struct rb_id_table *tbl, rb_id_table_foreach_func_t *func, void *data);
+void rb_id_table_foreach_with_replace(struct rb_id_table *tbl, rb_id_table_foreach_func_t *func, rb_id_table_update_callback_func_t *replace, void *data);
+void rb_id_table_foreach_values(struct rb_id_table *tbl, rb_id_table_foreach_values_func_t *func, void *data);
+
+#endif	/* RUBY_ID_TABLE_H */
